c161d524559914e4d8ef1dc9e9de55c64b418df9
bst003 <bstanner33@yahoo.com>
2024-01-06 01:08:35 -0500

    Begin testing readFile functionality

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 console.log("test");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+// const data = fs.readFile(
+//   path.join(__dirname, "/public", "about.html"),
+//   "utf8",
+//   (err, data) => {
+//     if (err) {
+//       throw err;
+//     }
+
+//     // console.log(data);
+//     return data;
+//   }
+// );
+
+const server = http
+  .createServer((req, res) => {
+    fs.readFile(path.join(__dirname, "/public", "about.html"), (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.write(data);
+      return res.end();
+    });
+
+    console.log(req);
+    // console.log("after req");
+
+    const url = req.url;
+    console.log(url);
+  })
+  .listen(5000, () => console.log("Server running..."));
