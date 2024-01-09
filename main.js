64ac68204722f919ecb6c30c47db1712a502971b
bst003 <bstanner33@yahoo.com>
2024-01-08 20:35:56 -0500

    Add nav and nav stylings

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
-console.log("test");
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// const data = fs.readFile(
-//   path.join(__dirname, "/public", "about.html"),
-//   "utf8",
-//   (err, data) => {
-//     if (err) {
-//       throw err;
-//     }
-
-//     // console.log(data);
-//     return data;
-//   }
-// );
-
 const server = http
   .createServer((req, res) => {
     let url = req.url === "/" ? "/index.html" : req.url;
-    console.log(url);
 
     const fileName = path.join(__dirname, "/public", url);
 
     const fileExt = path.extname(fileName);
-
-    console.log(fileExt);
 
     let contentType;
     if (fileExt === ".html") {
@@ -41,7 +24,6 @@ const server = http
     fs.readFile(fileName, "utf8", (err, data) => {
       // In case of error
       if (err) {
-        console.log("does this run");
         fs.readFile(
           path.join(__dirname, "/public", "/404.html"),
           "utf8",
